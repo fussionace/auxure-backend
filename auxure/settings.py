dd353663b2da6a1d088fddf9b66acc3a56c4758d
adetunji peter <peteradetunji30@gmail.com>
2023-07-02 16:18:59 +0100

    modified the store admin

--- auxure/settings.py
+++ auxure/settings.py
@@ -129,6 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+    'PAGE_SIZE' : 3,
     'DEFAULT_PERMISSION_CLASSES': [
    'rest_framework.permissions.AllowAny',
 ]
--- store/admin.py
+++ store/admin.py
@@ -6,7 +6,5 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'description', 'discount', 'image', 'price', 'slug', 'inventory', 'top_deal', 'flash_sales']
 
 admin.site.register(Perfume, ProductAdmin)
-admin.site.register(Category)
-admin.site.register(Cartitems)
-admin.site.register(Cart)
+
 
