2fbae8b2bcda113eebcdbde62e2e343239e0f3f8
David Omsiakin <davidomisakin4good@gmail.com>
2023-07-02 12:53:26 +0100

    Fixed the conflict in the api app

--- api/urls.py
+++ api/urls.py
@@ -27,7 +27,7 @@ router.register("users", UserProfileviewSet)
 # Child routers
 # perfume child router
 perfume_router = routers.NestedDefaultRouter(router, "perfumes", lookup="perfume")
-# perfume_router.register("reviews", views.ReviewViewSet, basename="perfume-reviews")
+perfume_router.register("reviews", views.ReviewViewSet, basename="perfume-reviews")
 
 # cart child router
 cart_router = routers.NestedDefaultRouter(router, "carts", lookup="cart")
@@ -36,7 +36,8 @@ cart_router.register("items", views.CartItemViewSet, basename="cart-items")
 users_router = routers.NestedDefaultRouter(router, "users", lookup="users")
 # register_router = routers.NestedDefaultRouter(router, "registers", lookup="registers")
 
-# http://127.0.0.1:8000/api/products/a28a9e75-8c04-4754-a931-7f945ab33550/reviews/
+# http://127.0.0.1:8000/api/perfumes/a28a9e75-8c04-4754-a931-7f945ab33550/reviews/  
+# The id within the url is for the specific product so the review will be automatically dropped for that product
 # http://127.0.0.1:8000/api/carts/
 
 urlpatterns = [
@@ -46,6 +47,5 @@ urlpatterns = [
     path("", include(users_router.urls)),
     path('signup/', CreateUser.as_view(), name='create-user'),
     path('login/', LoginView.as_view(), name='login'),
-    # path("", include(register_router.urls)),
-    # path('api/', include()
+
 ]
--- auxure/settings.py
+++ auxure/settings.py
@@ -130,12 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-
     'PAGE_SIZE': 3
 }
 
