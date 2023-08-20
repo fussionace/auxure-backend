e3b2f63c2f6f425966822b4ef9c7033389b892cd
NsikanPatrick <nsikanpatrick69@gmail.com>
2023-08-20 15:41:44 +0200

    Configuring environment variables

--- auxure/settings.py
+++ auxure/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from decouple import config
 
 from pathlib import Path
 from datetime import timedelta
@@ -19,14 +20,19 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Environment variables
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+GOOGLE_CLIENT_ID = config('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
+GOOGLE_SECRET = config('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9do86p0gu0(-3842o!s#+8+lf!++hl4^4!9_g1=ucug31unf7h'
+# SECRET_KEY = '################################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 # DEBUG = False
 
 ALLOWED_HOSTS = ['*']
@@ -195,8 +201,8 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '883465917784-82atmsidsdqevjdtgvbmlj8bffi1gd1v.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+# SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '################################'
+# SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '################################################################'
 
 # Social Redirect URLs
 LOGIN_URL = 'login'
